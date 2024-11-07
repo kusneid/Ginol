ba731a9d3fee32a7a0371e0900cfd05ae02881c9
kusneid <kusneid@gmail.com>
2024-11-08 01:43:13 +0300

    encryption added

--- backend/main.go
+++ backend/main.go
@@ -4,32 +4,31 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"Ginol/user"
 )
-
-type User struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
-
-type Credentials struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
 
 func main() {
 	r := gin.Default()
 
 	r.POST("/api/register", func(c *gin.Context) {
-		var user User
-		if err := c.ShouldBindJSON(&user); err != nil {
+		var userAdded user.User
+		if err := c.ShouldBindJSON(&userAdded); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid data"})
 			return
 		}
+
+		if err := userAdded.Crypt(); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to encrypt password"})
+			return
+		}
+
+		//fmt.Print(userAdded.Username, userAdded.Password) для теста
 		c.JSON(http.StatusOK, gin.H{"message": "User registered successfully"})
 	})
 
 	r.POST("/api/login", func(c *gin.Context) {
-		var credentials Credentials
+		var credentials user.Credentials
 		if err := c.ShouldBindJSON(&credentials); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid data"})
 			return
--- backend/user/auth.go
+++ backend/user/auth.go
@@ -1,2 +1,30 @@
 package user
+
 // аутенфикация (почему-то гпт настоятельно рекомендует ее отдельно делать)
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func (u *User) Crypt() error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+	return nil
+}
+
+func (u *User) ServerDeliver() error {
+	//с помощью pgx
+	return nil
+}
